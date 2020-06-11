7db806b7b8f7b83021d9312dd0b1dd2c3afccdf0
GabrielTennent <gabrieljrtennent@gmail.com>
2020-06-12 01:34:31 +1200

    Started working further on get and do methods still nw

--- src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
@@ -1,37 +1,48 @@
 package nz.ac.vuw.swen301.a2.server;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class LogsServlet extends HttpServlet {
 
+    private ArrayList list = new ArrayList();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doGet(req, resp);
-        //String userAgentHeader = req.getHeader( "User-Agent");
-
         resp.setContentType("application/json");
         String limitReq = req.getParameter("limit");
         String levelReq = req.getParameter("level");
 
-        //String request = req.toString();
+        JsonObject jsonObject = null;
 
-//        try{
-//        int limit = Integer.parseInt(limitReq);
-//
-//        for (int i = 0; i < limit; i++){
-//            if(list.size() >= limit) {
-//                Object s = list.get(i);
-//            }
-//        }
-//        } catch (NumberFormatException x){
-//            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-//            return;
-//        }
+        try{
+        int limit = Integer.parseInt(limitReq);
+
+        for (int i = 0; i < limit; i++){
+            if(list.size() >= limit) {
+                Object s = list.get(i);
+                StringBuffer sb = new StringBuffer();
+                String cl = null;
+                BufferedReader reader = req.getReader();
+                while((cl = reader.readLine()) != null){
+                    sb.append(cl);
+                }
+                jsonObject = JsonParser.parseString(sb.toString()).getAsJsonObject();
+            }
+        }
+        } catch (NumberFormatException x){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
 
         resp.setContentType("text/html");
         //resp.setContentType("plain/text");
@@ -40,9 +51,8 @@ public class LogsServlet extends HttpServlet {
         out.println("<body>");
         out.println("Search Query: <br>"
                 + "limit: " + limitReq + "<br>"
-                + "level: " + levelReq);
-        //out.println("");
-        //Print line for displaying on the screen
+                + "level: " + levelReq + "<br>");
+        //out.println("<br> Matching Logs: " + jsonObject.toString());
         out.println("</body>");
         out.println("</html>");
         //out.close();
@@ -50,6 +60,6 @@ public class LogsServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        list.add(req.getReader());
     }
 }
