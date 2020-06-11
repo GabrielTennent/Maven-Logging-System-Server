d4e4988dfd8b303d6faed1b16bfdb88119582092
GabrielTennent <gabrieljrtennent@gmail.com>
2020-06-12 01:02:39 +1200

    changed server url stuff

--- src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
@@ -12,18 +12,40 @@ public class LogsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
-        String userAgentHeader = req.getHeader( "User-Agent");
+        //String userAgentHeader = req.getHeader( "User-Agent");
 
+        resp.setContentType("application/json");
+        String limitReq = req.getParameter("limit");
+        String levelReq = req.getParameter("level");
 
-        PrintWriter out = resp.getWriter();
+        //String request = req.toString();
+
+//        try{
+//        int limit = Integer.parseInt(limitReq);
+//
+//        for (int i = 0; i < limit; i++){
+//            if(list.size() >= limit) {
+//                Object s = list.get(i);
+//            }
+//        }
+//        } catch (NumberFormatException x){
+//            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+
         resp.setContentType("text/html");
-
-
+        //resp.setContentType("plain/text");
+        PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<body>");
-        out.println("the user agent string is " + userAgentHeader);
+        out.println("Search Query: <br>"
+                + "limit: " + limitReq + "<br>"
+                + "level: " + levelReq);
+        //out.println("");
+        //Print line for displaying on the screen
         out.println("</body>");
         out.println("</html>");
+        //out.close();
     }
 
     @Override
